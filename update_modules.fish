90ddbf7fd74a850b4b2bb54f6d4bbaa7689a7eae
deigote <diego@toharia.com>
2014-05-19 19:02:29 +0200

    Changed the declaration of modules to be based on a function instead of being funky variables

--- update_modules.fish
+++ update_modules.fish
@@ -16,6 +16,26 @@ function get_modules_definitions_file
 	echo (get_fish_home)/modules_config.fish
 end
 
+function module
+	set modules_home (get_modules_home)
+	set module_def $argv_name
+	set module_repo $argv[1]
+	set module_path "$modules_home/$argv[2]"
+	if test (count $argv) -gt 2
+		set module_branch $argv[3]
+	else
+		set module_branch master
+	end
+	echo '###'
+	if test -d $module_path
+		echo "Pulling $module_path"
+		cd $module_path ;and git pull; cd -
+	else
+		echo "Cloning $module_repo (branch $module_branch) into $module_path"
+		git clone $module_repo -b $module_branch $module_path
+	end
+end
+
 function update_modules
 	set modules_definitions_file (get_modules_definitions_file)
 	set modules_home (get_modules_home)
@@ -23,24 +43,6 @@ function update_modules
 		mkdir "$modules_home"
 	end
 	source "$modules_definitions_file"
-	for module_def_name in (set -n | grep "^fish_module_")
-		set module_def $$module_def_name
-		set module_repo $module_def[1]
-		set module_path "$modules_home/$module_def[2]"
-		if test (count $module_def) -gt 2
-			set module_branch $module_def[3]
-		else
-			set module_branch master
-		end
-		echo '###'
-		if test -d $module_path
-			echo "Pulling $module_path"
-			cd $module_path ;and git pull; cd -
-		else
-			echo "Cloning $module_repo (branch $module_branch) into $module_path"
-			git clone $module_repo -b $module_branch $module_path
-		end
-	end
 end
 
 update_modules
