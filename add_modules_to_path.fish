e93becc04e8f07ed996e4e83f3c82e98ad48a51a
deigote <diego@toharia.com>
2014-05-19 19:02:19 +0200

    Fixed path to search for the modules

--- add_modules_to_path.fish
+++ add_modules_to_path.fish
@@ -1,14 +1,10 @@
-function get_fish_home
-        if test "$XDG_CONFIG_HOME" != ""
-                echo $XDG_CONFIG_HOME
-        else
-                echo "$HOME/.config/fish"
-        end
-end
-
 function add_modules_to_path
-	set fish_home (get_fish_home)
-	for module_dir in (find $fish_home -type d -mindepth 1 -maxdepth 1) 
+        if test "$XDG_CONFIG_HOME" != ""
+                set fish_home "$XDG_CONFIG_HOME"
+        else
+                set fish_home "$HOME/.config/fish"
+        end
+	for module_dir in (find $fish_home/modules -type d -mindepth 1 -maxdepth 1)
 		if test -d $module_dir/functions
 			set fish_function_path $fish_function_path $module_dir/functions
 		end
@@ -17,5 +13,4 @@ function add_modules_to_path
 		end
 	end
 end
-
 add_modules_to_path
